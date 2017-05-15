630458db7de778f611391d9833427c3ec2336e84
smb564 <supun.14@cse.mrt.ac.lk>
2017-05-15 22:08:27 +0530

    Opportunity portal main view partially done

--- src/main/webapp/app/Pages/OpportunityPortal/opportunity-portal.controller.js
+++ src/main/webapp/app/Pages/OpportunityPortal/opportunity-portal.controller.js
@@ -5,13 +5,48 @@
         .module("uniConnectApp")
         .controller("OpportunityPortalController", OpportunityPortalController);
 
-    OpportunityPortalController.$inject = ['Opportunity'];
+    OpportunityPortalController.$inject = ['Opportunity', 'INTEREST_FIELDS', 'DEPARTMENTS'];
 
-    function OpportunityPortalController(Opportunity){
+    function OpportunityPortalController(Opportunity, INTEREST_FIELDS, DEPARTMENTS){
         var vm = this;
 
-        vm.hello = "Hello from the controller!";
+        // Object sent to filter out opportunities
+        vm.OpportunityFilter = {
+            'targets' : [],
+            'tags' : []
+        };
 
-    };
+        // methods used to maniputale targets
+        vm.addTarget = addTarget;
+        vm.removeTarget = removeTarget;
+
+        // Get the interests and departments. Generate semesters
+        vm.interests = INTEREST_FIELDS;
+        vm.departments = DEPARTMENTS;
+
+
+
+        vm.semesters = [];
+
+        for(var i = 1; i < 9; i++){
+            vm.semesters.push(i);
+        }
+
+        // Set selected for select
+        vm.targetSem = vm.semesters[0];
+        vm.targetDep = vm.departments[0];
+
+        function addTarget(){
+            // Check for duplicates and add
+            for(var i=0; i < vm.OpportunityFilter.targets.length ; i++){
+                if (vm.OpportunityFilter.targets[i][0]===vm.targetSem && vm.OpportunityFilter.targets[i][1]===vm.targetDep){ return ; }
+            }
+            vm.OpportunityFilter.targets.push([vm.targetSem, vm.targetDep]);
+        }
+
+        function removeTarget(index){
+            vm.OpportunityFilter.targets.splice(index, 1);
+        }
+    }
 
 })();
